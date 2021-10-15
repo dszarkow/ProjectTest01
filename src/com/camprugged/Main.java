fa9c49117f2e01cff1fd955c978c70e8d1d12acc
Donald Szarkowicz <donald.szarkowicz@navy.mil>
2021-10-15 05:58:58 -0400

    Initial commit

---
+++ src/com/camprugged/Main.java
@@ -0,0 +1,16 @@
+// ProjectTest1
+//
+// D. Szarkowicz
+// October, 2021
+
+package com.camprugged;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Person p = new Person();
+
+        System.out.println("Person's name is: " + p.name);
+    }
+}
---
+++ src/com/camprugged/Person.java
@@ -0,0 +1,5 @@
+package com.camprugged;
+
+public class Person {
+    public String name = "Don";
+}
